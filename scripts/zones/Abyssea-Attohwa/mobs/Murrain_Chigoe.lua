46c14064f81c8209ab19c87a41b469369f8a0a51
TeoTwawki <arawn.annwn@yandex.com>
2018-05-19 11:44:03 -0400

    Add insta death to crit hits and ws hits in non NM chigoe scripts. chigoe insta killed in this way give no exp. Insta death only triggers if the attack would not empty its hp bar, according to what I have been told.
    Todo's:
     - DNC step also insta kills chigoe.
     - message displayed on death is the "falls down" message instead of regular defeat message.
    
    Fixes #4878

--- scripts/zones/Abyssea-Attohwa/mobs/Murrain_Chigoe.lua
+++ scripts/zones/Abyssea-Attohwa/mobs/Murrain_Chigoe.lua
@@ -4,6 +4,22 @@
 -- Note: This mob does not need chigoe mixin
 -- TODO: En-Disease
 -----------------------------------
+require("scripts/globals/status")
+-----------------------------------
+
+function onCriticalHit(mob)
+    if mob:getHP() > 0 then
+        mob:setMobMod(dsp.mobMod.EXP_BONUS, -100)
+        mob:setHP(0)
+    end
+end
+
+function onWeaponskillHit(mob, attacker, weaponskill)
+    if mob:getHP() > 0 then
+        mob:setMobMod(dsp.mobMod.EXP_BONUS, -100)
+        mob:setHP(0)
+    end
+end
 
 function onMobDeath(mob, player, isKiller)
 end
--- scripts/zones/The_Eldieme_Necropolis_[S]/mobs/Chigoe.lua
+++ scripts/zones/The_Eldieme_Necropolis_[S]/mobs/Chigoe.lua
@@ -2,20 +2,38 @@
 -- Area: The Eldieme Necropolis (S) (175)
 --  MOB: Chigoe
 -----------------------------------
-
--- -----------------------------------
+mixins = { require("scripts/mixins/families/chigoe") }
+require("scripts/globals/status")
+-----------------------------------
 
 function onMobInitialize(mob)
-end;
+end
 
 function onMobSpawn(mob)
-end;
+end
 
-function onMobEngaged(mob,target)
-end;
+function onMobEngaged(mob, target)
+end
 
 function onMobFight(mob,target)
-end;
+end
+
+function onMobDisengage(mob)
+end
+
+function onCriticalHit(mob)
+    if mob:getHP() > 0 then
+        mob:setMobMod(dsp.mobMod.EXP_BONUS, -100)
+        mob:setHP(0)
+    end
+end
+
+function onWeaponskillHit(mob, attacker, weaponskill)
+    if mob:getHP() > 0 then
+        mob:setMobMod(dsp.mobMod.EXP_BONUS, -100)
+        mob:setHP(0)
+    end
+end
 
 function onMobDeath(mob, player, isKiller)
-end;
+end
